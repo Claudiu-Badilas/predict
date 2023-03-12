0e160f4e6bd24e6978c4fa85f672e9e2bd2e6889
claudiu_badilas <Claudiu.Badilas@napa.fi>
2023-03-12 11:57:19 +0200

    fix acc apis

--- DataAnalysis.Repository/Repositories/UserRepository.cs
+++ DataAnalysis.Repository/Repositories/UserRepository.cs
@@ -10,7 +10,9 @@ namespace DataAnalysis.Repositories {
         public async Task<bool> IsExistingUser(string email) {
             using (var connection = new NpgsqlConnection(NpsqlConnectionString)) {
                 connection.Open();
-                var sql = @"SELECT email FROM platform.users WHERE email = @email;";
+                var sql = @"
+                    SELECT email FROM public.""user"" 
+                    WHERE email = @email;";
 
                 return (await connection.QueryAsync<string>(sql, new { email })).ToList().Count > 0;
             };
@@ -29,7 +31,7 @@ namespace DataAnalysis.Repositories {
                         u.last_login as LastLogin,
                         u.is_active as IsActive,
                         u.is_admin as IsAdmin
-                    FROM public.users u 
+                    FROM public.""user"" u 
                     WHERE u.email = @email;";
                 return (await connection.QueryAsync<AppUser>(sql, new { email })).FirstOrDefault();
             };
@@ -39,7 +41,7 @@ namespace DataAnalysis.Repositories {
             using (var connection = new NpgsqlConnection(NpsqlConnectionString)) {
                 connection.Open();
                 var sql = @"
-                    INSERT INTO public.users 
+                    INSERT INTO public.""user"" 
                         (email, password_hash, password_salt, join_date, last_login, is_active, is_admin) 
                     VALUES
                         (@Email, @PasswordHash, @PasswordSalt, @JoinDate, @LastLogin, @IsActive, @IsAdmin);";
