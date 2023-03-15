817ea6607e45242d7d2851b6f7c51c5c2d6ea894
Claudiu <claudiu.badilas@gmail.com>
2023-03-15 21:51:07 +0200

    add register form and UI

--- DataAnalysis.Front-end/src/app/platform/authentication/login-register/login-register.component.ts
+++ DataAnalysis.Front-end/src/app/platform/authentication/login-register/login-register.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  UntypedFormGroup,
-} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { CustomValidators } from 'src/app/shared/validators/custom-validator';
+import { AuthenticationAction } from './models/authentication-actions.enum';
 
 @Component({
   selector: 'app-login-register',
@@ -14,28 +10,67 @@ import { CustomValidators } from 'src/app/shared/validators/custom-validator';
 })
 export class LoginRegisterComponent {
   credentialForm: FormGroup;
+  registerMessage = `You do not have an account, start journey with us from`;
+  loginMessage = `You already have an account, just log in from`;
 
   constructor(private formBuilder: FormBuilder) {
     this.credentialForm = this.formBuilder.group({
+      authenticationAction: new FormControl(AuthenticationAction.Login),
+      authenticationMessage: new FormControl(this.registerMessage),
       email: new FormControl(null, [CustomValidators.email]),
       password: new FormControl(null, [CustomValidators.minLength(4)]),
     });
   }
 
-  isValidateInputValue(form: FormGroup, inputName: string) {
+  isValidateInputValue(inputName: string) {
     return (
-      form.get(inputName).invalid &&
-      (!form.get(inputName).pristine || form.get(inputName).dirty)
+      this.credentialForm.get(inputName).invalid &&
+      (!this.credentialForm.get(inputName).pristine ||
+        this.credentialForm.get(inputName).dirty)
     );
   }
 
-  getInputValidationError(form: FormGroup, inputName: string) {
-    return form.controls[inputName]?.errors['value'];
+  getInputValidationError(inputName: string) {
+    return this.credentialForm.controls[inputName]?.errors['value'];
   }
 
-  onSubmitChanges(form: UntypedFormGroup) {
-    console.log('invalid', form.get('email').invalid);
-    console.log('pristine', !form.get('email').pristine);
-    console.log('dirty', form.get('email').dirty);
+  getInputValue(inputName: string) {
+    return this.credentialForm.controls[inputName]?.value;
+  }
+
+  onSubmitChanges(form: FormGroup) {
+    console.log('form', form.controls);
+  }
+
+  onChangeAuthAction() {
+    if (
+      this.getInputValue('authenticationAction') === AuthenticationAction.Login
+    ) {
+      this.credentialForm.addControl(
+        'confirmPassword',
+        new FormControl(null, [CustomValidators.minLength(4)])
+      );
+      this.credentialForm.reset();
+      this.credentialForm.patchValue({
+        ...this.credentialForm,
+        authenticationAction: AuthenticationAction.Register,
+        authenticationMessage: this.loginMessage,
+      });
+    } else {
+      this.credentialForm.removeControl('confirmPassword');
+      this.credentialForm.reset();
+      this.credentialForm.patchValue({
+        ...this.credentialForm,
+        authenticationAction: AuthenticationAction.Login,
+        authenticationMessage: this.registerMessage,
+      });
+    }
+  }
+
+  isRegisterFromSelected() {
+    return (
+      this.getInputValue('authenticationAction') ===
+      AuthenticationAction.Register
+    );
   }
 }
