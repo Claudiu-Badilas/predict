96e736d087ffc70edbb4634088257c68378381f0
Claudiu <claudiu.badilas@gmail.com>
2023-04-16 00:13:10 +0300

    add InitialReceiptTables_202304150002 migration

--- DataAnalysis.Back-end/Configuration/Migrations/InitialReceiptTables_202304150003.cs
+++ DataAnalysis.Back-end/Configuration/Migrations/InitialReceiptTables_202304150003.cs
@@ -4,11 +4,39 @@ namespace DataAnalysis.Configuration.Migrations {
     [Migration(202304150003)]
     public class InitialReceiptTables_202304150003 : Migration {
         public override void Down() {
-            throw new NotImplementedException();
+            Delete.Table("purchased_product2");
+            Delete.Table("quantity_type2");
+            Delete.Table("receipt2");
         }
 
         public override void Up() {
-            throw new NotImplementedException();
+            Create.Table("quantity_type2")
+                .WithColumn("id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("type").AsString().NotNullable().Unique();
+            Insert.IntoTable("quantity_type2")
+                .Row(new { id = 1, type = "BUC" })
+                .Row(new { id = 2, type = "KG" });
+
+            Create.Table("receipt2")
+                .WithColumn("id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("identifier").AsString().NotNullable().Unique()
+                .WithColumn("date").AsDateTime()
+                .WithColumn("total_price").AsDouble()
+                .WithColumn("total_discount").AsDouble()
+                .WithColumn("description").AsString()
+                .WithColumn("provider_id").AsInt32().NotNullable().ForeignKey("provider2", "id")
+                .WithColumn("currency_id").AsInt32().NotNullable().ForeignKey("currency2", "id")
+                .WithColumn("data_owner_id").AsInt32().NotNullable().ForeignKey("data_owner2", "id");
+
+            Create.Table("purchased_product2")
+                .WithColumn("id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("name").AsString()
+                .WithColumn("price").AsDouble()
+                .WithColumn("quantity").AsInt32()
+                .WithColumn("vat").AsInt32()
+                .WithColumn("quantity_type_id").AsInt32().NotNullable().ForeignKey("quantity_type2", "id")
+                .WithColumn("receipt_id").AsInt32().NotNullable().ForeignKey("receipt2", "id");
+
         }
     }
 }
