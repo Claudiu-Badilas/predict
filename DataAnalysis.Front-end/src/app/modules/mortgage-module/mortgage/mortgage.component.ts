fc8ffe1761912800dd37a58bce80411c577916ae
claudiu.badilas <claudiu.badilas@gmail.com>
2025-12-07 19:49:57 +0200

    add anticipate calc

--- DataAnalysis.Front-end/src/app/modules/mortgage-module/mortgage/mortgage.component.ts
+++ DataAnalysis.Front-end/src/app/modules/mortgage-module/mortgage/mortgage.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { first } from 'rxjs';
-import { GraficRambursare } from '../models/mortgage.model';
+import { GraficRambursare, Rata } from '../models/mortgage.model';
 import { MortgageService } from '../services/mortgage.service';
 
 @Component({
@@ -17,6 +17,55 @@ export class MortgageComponent {
       .pipe(first())
       .subscribe((res) => {
         this.transactions = res;
+        // this.rateSelectate = res[0].rate.slice(0, 7);
       });
+  }
+
+  rateSelectate: Rata[] = [];
+  showDetails = false;
+
+  toggleDetails() {
+    this.showDetails = !this.showDetails;
+  }
+
+  onSelect(rata: Rata) {
+    const index = this.rateSelectate.findIndex((r) => r.nrCtr === rata.nrCtr);
+
+    if (index !== -1) {
+      this.rateSelectate.splice(index, 1);
+    } else {
+      this.rateSelectate.push(rata);
+    }
+
+    this.rateSelectate = this.rateSelectate.sort((a, b) => a.nrCtr - b.nrCtr);
+  }
+
+  get anySelected(): boolean {
+    return this.rateSelectate.length > 0;
+  }
+
+  get rata() {
+    return this.rateSelectate[0] || null;
+  }
+
+  get anticipate() {
+    return this.rateSelectate.slice(1);
+  }
+
+  get totalAnticipate() {
+    return (this.anticipate ?? [])
+      .map((a) => a.rataCredit)
+      .reduce((sum, val) => sum + val, 0);
+  }
+
+  get totalDobandaSalvata() {
+    return (this.anticipate ?? []).reduce(
+      (sum, val) => sum + (val.totalRata - val.rataCredit),
+      0
+    );
+  }
+
+  get total() {
+    return (this.rata?.totalRata ?? 0) + this.totalAnticipate;
   }
 }
