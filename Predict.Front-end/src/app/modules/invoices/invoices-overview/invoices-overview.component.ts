dc7d79001e5e7afadd6b90df0d7ddd232a897736
claudiu.badilas <claudiu.badilas@gmail.com>
2026-01-05 00:33:40 +0200

    use local reducer on child comp

--- Predict.Front-end/src/app/modules/invoices/invoices-overview/invoices-overview.component.ts
+++ Predict.Front-end/src/app/modules/invoices/invoices-overview/invoices-overview.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import * as fromMortgageLoan from 'src/app/modules/mortgage-loan/state-management/mortgage-loan.reducer';
+import * as fromInvoices from 'src/app/modules/invoices/reducers/invoices.reducer';
 import { SideBarModule } from 'src/app/shared/components/side-bar/side-bar.module';
 import { ToggleButtonComponent } from 'src/app/shared/components/toggle-button/toggle-button.component';
 import * as NavigationAction from 'src/app/store/actions/navigation.actions';
@@ -12,7 +12,7 @@ import * as NavigationAction from 'src/app/store/actions/navigation.actions';
   styleUrl: './invoices-overview.component.scss',
 })
 export class InvoicesOverviewComponent {
-  constructor(private store: Store<fromMortgageLoan.MortgageLoanState>) {}
+  constructor(private store: Store<fromInvoices.State>) {}
 
   onSelectionChange(module: string) {
     this.store.dispatch(
