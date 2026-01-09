a11669d2ec61c5535324fa1a84dcf8d58079fd87
claudiu.badilas <claudiu.badilas@gmail.com>
2026-01-09 10:44:03 +0200

    update project name from backend to API

---
+++ Predict.API/Controllers/TransactionController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Predict.Repository.TransactionRepo;
+using Predict.Service.AuthorizationService;
+using System.ComponentModel.DataAnnotations;
+
+namespace Predict.Controllers;
+
+[Route("api/v1")]
+public class TransactionController : BaseController
+{
+
+    private readonly ITransactionRepo _transactionRepo;
+    private readonly IAuthService _authService;
+
+    public TransactionController(ITransactionRepo transactionRepo, IAuthService authService)
+    {
+        _transactionRepo = transactionRepo;
+        _authService = authService;
+    }
+
+    [HttpGet("transactions/{dataOwnerId}")]
+    [Authorize]
+    public async Task<ActionResult> GetTransactions(
+        [FromHeader] string Authorization,
+        [FromRoute] int dataOwnerId
+        )
+    {
+
+        var user = await _authService.GetUser(Authorization);
+        if (!user.HasAccessToDataOwner(dataOwnerId))
+        {
+            return BadRequest("You do not have access to the current Data Owner!");
+        }
+
+        return Ok(await _transactionRepo.GetTransactionByUserIdAndOwnerId(user.Id.Value, dataOwnerId, DateTime.UtcNow, DateTime.UtcNow));
+    }
+
+    [HttpGet("free-transactions/{dataOwnerId}")]
+    public async Task<ActionResult> GetFreeTransactions(
+        [FromHeader] string Authorization,
+        [FromRoute] int dataOwnerId,
+        [FromQuery, Required] string startDate,
+        [FromQuery, Required] string endDate
+    )
+    {
+
+        return Ok(await _transactionRepo.GetTransactionByUserIdAndOwnerId(1, dataOwnerId, DateTime.Parse(startDate), DateTime.Parse(endDate)));
+    }
+}
--- Predict.Back-end/Controllers/BaseController .cs
+++
@@ -1,6 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Predict.Controllers {
-    [ApiController]
-    public class BaseController : ControllerBase { }
-}
