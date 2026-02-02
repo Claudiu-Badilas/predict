b066d7a98fa6bf601fff9a20db9859ad67fc6187
claudiu.badilas <claudiu.badilas@gmail.com>
2026-02-02 08:38:07 +0200

    update model

--- Predict.Client/src/app/modules/mortgage-loan/mortgage-loan-detailed/models/base-loan-rate.model.ts
+++ Predict.Client/src/app/modules/mortgage-loan/mortgage-loan-detailed/models/base-loan-rate.model.ts
@@ -3,7 +3,24 @@ export type HistocialInstalmentPayment = {
   paymentDate: Date;
   principalAmount: number;
   interestAmount: number;
+  insuranceCost: number;
   remainingBalance: number;
   instalmentPayment: boolean;
   earlyPayment: boolean;
 };
+
+export class HistocialInstalmentPaymentBatch {
+  public completed: boolean = false;
+  public expanded: boolean = true;
+  public id: number;
+  public title: Date;
+
+  constructor(public instalments: HistocialInstalmentPayment[]) {
+    if (!instalments.length) return;
+
+    this.completed = instalments.some((i) => i.instalmentPayment);
+    this.expanded = !this.completed;
+    this.id = instalments[0]?.index ?? 0;
+    this.title = instalments[0].paymentDate ?? null;
+  }
+}
