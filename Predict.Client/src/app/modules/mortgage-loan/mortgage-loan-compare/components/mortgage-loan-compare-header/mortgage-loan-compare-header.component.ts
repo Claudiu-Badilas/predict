41fc2d3692a03c5ff3209a442a5997d0cf4df478
claudiu.badilas <claudiu.badilas@gmail.com>
2026-02-04 20:26:00 +0200

    add header compare

---
+++ Predict.Client/src/app/modules/mortgage-loan/mortgage-loan-compare/components/mortgage-loan-compare-header/mortgage-loan-compare-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mortgage-loan-compare-header',
+  imports: [],
+  templateUrl: './mortgage-loan-compare-header.component.html',
+  styleUrl: './mortgage-loan-compare-header.component.scss',
+})
+export class MortgageLoanCompareHeaderComponent {
+
+}
