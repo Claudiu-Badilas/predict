69e0b68264f98cb736a1262ba48c49d477165413
claudiu.badilas <claudiu.badilas@gmail.com>
2026-02-04 21:51:15 +0200

    add compare body template

---
+++ Predict.Client/src/app/modules/mortgage-loan/mortgage-loan-compare/components/mortgage-loan-compare-body/mortgage-loan-compare-body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mortgage-loan-compare-body',
+  imports: [],
+  templateUrl: './mortgage-loan-compare-body.component.html',
+  styleUrl: './mortgage-loan-compare-body.component.scss',
+})
+export class MortgageLoanCompareBodyComponent {
+
+}
