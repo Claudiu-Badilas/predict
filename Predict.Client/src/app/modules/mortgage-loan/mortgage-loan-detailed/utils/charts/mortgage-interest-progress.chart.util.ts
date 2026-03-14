b6f779e3fe4ebe332ff6b52c6f3bf30823d3acbf
claudiu.badilas <claudiu.badilas@gmail.com>
2026-03-14 18:41:09 +0200

    update charts

--- Predict.Client/src/app/modules/mortgage-loan/mortgage-loan-detailed/utils/charts/mortgage-interest-progress.chart.util.ts
+++ Predict.Client/src/app/modules/mortgage-loan/mortgage-loan-detailed/utils/charts/mortgage-interest-progress.chart.util.ts
@@ -1,7 +1,9 @@
+import { SeriesOptionsType } from 'highcharts';
+import { NumberFormatPipe } from 'src/app/shared/pipes/number-format.pipe';
+import { Colors } from 'src/app/shared/styles/colors';
 import { Calculator } from 'src/app/shared/utils/calculator.utils';
 import { MathUtil } from 'src/app/shared/utils/math.utils';
 import { HistoricalInstalmentPayment } from '../../models/base-loan-rate.model';
-import { Colors } from 'src/app/shared/styles/colors';
 
 export namespace MortgageInterestProgressChartUtils {
   export function getChart(
@@ -56,28 +58,36 @@ export namespace MortgageInterestProgressChartUtils {
     return {
       chart: {
         type: 'pie',
+        spacing: [20, 20, 20, 20],
       },
-
       title: {
-        text: 'Mortgage Overview',
+        text: `Overview (${NumberFormatPipe.numberFormat(total)})`,
         align: 'left',
+        style: { fontFamily: 'Inter, system-ui, sans-serif' },
       },
-
       tooltip: {
         headerFormat: '',
         pointFormat:
           '<span style="color:{point.color}">●</span> <b>{point.name}</b><br/>' +
           '<b>{point.y}%</b><br/>' +
-          'Amount: <b>{point.amount} RON</b>',
+          'Amount: <b>{point.amountCompact} RON</b>',
       },
-
       plotOptions: {
         pie: {
-          innerSize: '55%',
+          innerSize: '65%',
           borderRadius: 5,
           dataLabels: {
             enabled: true,
-            format: '{point.name}: {point.y}%',
+            format:
+              '<b>{point.nameShort}</b>: {point.amountCompact} ({point.y}%)',
+            style: {
+              fontSize: '11px',
+              textOutline: 'none',
+              fontWeight: 'normal',
+            },
+            connectorWidth: 1,
+            connectorPadding: 5,
+            distance: 20,
           },
         },
       },
@@ -88,43 +98,55 @@ export namespace MortgageInterestProgressChartUtils {
           data: [
             {
               name: 'Principal Platit',
+              nameShort: 'Pr. Platit', // Shortened name
               y: percent(paidPrincipal),
               amount: MathUtil.round(paidPrincipal),
+              amountCompact: NumberFormatPipe.numberFormat(paidPrincipal),
               color: Colors.TEAL_400,
             },
             {
               name: 'Principal Neplatit',
+              nameShort: 'Pr. Neplatit',
               y: percent(unpaidPrincipal),
               amount: MathUtil.round(unpaidPrincipal),
+              amountCompact: NumberFormatPipe.numberFormat(unpaidPrincipal),
               color: Colors.BS_DANGER,
             },
             {
               name: 'Dobanda Platita',
+              nameShort: 'Dob. Platita',
               y: percent(paidInterest),
               amount: MathUtil.round(paidInterest),
+              amountCompact: NumberFormatPipe.numberFormat(paidInterest),
               color: Colors.BLUE_400,
             },
             {
               name: 'PAD Platita',
+              nameShort: 'PAD',
               y: percent(paidInsurance),
               amount: MathUtil.round(paidInsurance),
+              amountCompact: NumberFormatPipe.numberFormat(paidInsurance),
               color: Colors.YELLOW_400,
             },
             {
               name: 'Dobanda Salvata',
+              nameShort: 'Dob. Salvata',
               y: percent(savedInterest),
               amount: MathUtil.round(savedInterest),
+              amountCompact: NumberFormatPipe.numberFormat(savedInterest),
               color: Colors.GREEN_400,
             },
             {
               name: 'Dobanda Neplatita',
+              nameShort: 'Dob. Neplatită',
               y: percent(unpaidInterest),
               amount: MathUtil.round(unpaidInterest),
+              amountCompact: NumberFormatPipe.numberFormat(unpaidInterest),
               color: Colors.BS_ORANGE,
             },
           ],
         },
-      ] as any[],
+      ] as SeriesOptionsType[],
     };
   }
 }
