2df0139f55023be2959dff731e6f2d16628ca796
claudiu.badilas <claudiu.badilas@gmail.com>
2026-04-26 13:06:29 +0300

    remove header

--- Predict.Client/src/app/modules/transaction/components/transaction-header/transaction-header.component.ts
+++ Predict.Client/src/app/modules/transaction/components/transaction-header/transaction-header.component.ts
@@ -1,57 +1,21 @@
-import { Component, computed, input, Signal } from '@angular/core';
-import { HeaderCardComponent } from 'src/app/shared/components/header-card/header-card.component';
-import {
-  CardSection,
-  HeaderCardInput,
-} from 'src/app/shared/components/header-card/models/header-card-input.model';
+import { Component, computed, input } from '@angular/core';
 import { TransactionDomain } from '../../models/transactions.model';
-import { Calculator } from 'src/app/shared/utils/calculator.utils';
+import { MonthlyMostCommonTransactionComponent } from '../monthly-most-common-transaction/monthly-most-common-transaction.component';
+import { MostCommonTransactionComponent } from '../most-common-transaction/most-common-transaction.component';
 
 @Component({
   selector: 'p-transaction-header',
-  imports: [HeaderCardComponent],
+  imports: [
+    MostCommonTransactionComponent,
+    MonthlyMostCommonTransactionComponent,
+  ],
   templateUrl: './transaction-header.component.html',
   styleUrls: ['./transaction-header.component.scss'],
 })
 export class TransactionHeaderComponent {
   readonly transactions = input.required<TransactionDomain[]>();
 
-  readonly incomes = computed(() =>
-    this.transactions()
-      .filter((t) => !t.ignored)
-      .filter((t) => t.amount > 0),
+  readonly ValidTransactions = computed(() =>
+    this.transactions().filter((t) => !t.ignored),
   );
-
-  readonly totalIncome = computed(() =>
-    Calculator.sum(this.incomes().map((a) => a.amount)),
-  );
-
-  readonly payments = computed(() =>
-    this.transactions()
-      .filter((t) => !t.ignored)
-      .filter((t) => t.amount < 0),
-  );
-
-  readonly totalPayment = computed(() =>
-    Calculator.sum(this.payments().map((a) => a.amount)),
-  );
-
-  readonly headerCardInputs: Signal<HeaderCardInput[]> = computed(() => [
-    {
-      sections: [
-        {
-          label: 'Total Incomes',
-          value: this.totalIncome(),
-          default: '0.00',
-          color: 'green',
-        } as CardSection,
-        {
-          label: 'Total Expenses',
-          value: this.totalPayment(),
-          default: '0.00',
-          color: 'red',
-        } as CardSection,
-      ],
-    } as HeaderCardInput,
-  ]);
 }
