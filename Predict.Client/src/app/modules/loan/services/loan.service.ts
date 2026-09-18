ebb3e41927f8af13a776247675e080f457f7e8bb
claudiu.badilas <claudiu.badilas@gmail.com>
2026-09-18 06:14:17 +0300

    update key

--- Predict.Client/src/app/modules/loan/services/loan.service.ts
+++ Predict.Client/src/app/modules/loan/services/loan.service.ts
@@ -6,7 +6,7 @@ import { LocalStorageService } from 'src/app/platform/services/local-storage.ser
 import { PrintoutsService } from 'src/app/platform/services/printouts.service';
 import { RepaymentSchedule, RepaymentScheduleDto } from '../models/loan.model';
 
-export const LoanService_STORAGE_KEY = 'GraficRambursare_18-Aug-2026';
+export const LoanService_STORAGE_KEY = 'GraficRambursare_18-Sep-2026';
 
 @Injectable({ providedIn: 'root' })
 export class LoanService {
